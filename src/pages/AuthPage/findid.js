f7d96a1f5956639d0d78154b964353fa1e6cbde2
moon-subin <sb020702@gmail.com>
2023-07-27 14:25:33 +0900

    FEAT: 로그인 페이지

---
+++ src/pages/AuthPage/findid.js
@@ -0,0 +1,7 @@
+function FindId() {
+    return (
+        <h1>FindId</h1>
+    );
+};
+  
+export default FindId;
--- src/pages/AuthPage/login.js
+++ src/pages/AuthPage/login.js
@@ -1,7 +1,96 @@
-function Auth() {
-  return (
-    <h1>Auth</h1>
-  );
-};
+import React from 'react';
+import { Link } from "react-router-dom";
+import './login.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import HorizonLine from '../../utils/horizontal_line.js';
 
-export default Auth;
+import {
+  Form,
+  Container,
+  Col
+} from 'react-bootstrap';
+
+
+function LogIn() {
+
+  const [state, setState] = React.useState({
+    email: "",
+    password: ""
+  });
+  const handleChange = event => {
+    const value = event.target.value;
+    setState({
+      ...state,
+      [event.target.name]: value
+    });
+  };
+
+  const handleOnSubmit = event => {
+    event.preventDefault();
+
+    const { email, password } = state;
+    alert(`You are login with email: ${email} and password: ${password}`);
+
+    for (const key in state) {
+      setState({
+        ...state,
+        [key]: ""
+      });
+    }
+  };
+
+  return (
+    <Container className="p-3 my-6 d-flex flex-column w-50">
+      <Form className='form-body'>    
+        <Col>
+          <img src={process.env.PUBLIC_URL + '/pitapet_dog_rmbg.png'} width = '75px'/>
+          <h1 class="h3 mb-4 fw-normal">로그인</h1>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="이메일을 입력하세요" />
+          </Form.Group>
+          <Form.Group className="mb-4" controlId="formBasicPassword">
+            <Form.Control type="password" placeholder="비밀번호를 입력하세요" />
+          </Form.Group>
+        </Col>
+
+        <div className="d-flex justify-content-between mb-4">
+          <div className='d-flex login-keep-box'>
+            <input type="checkbox" className='checkbox' />
+              로그인 유지
+          </div>
+          <Link to={"/findpwd"} className="findpwd-link">
+            비밀번호 찾기
+          </Link>
+        </div>
+
+        <button type="submit" className="w-100 btn-lg mb-4 loginBtn">
+          이메일로 로그인
+        </button>
+
+        <div className='mx-auto mb-4 vr-form' style={{ width: '30%' }}>
+          <Link to={"/signup"} className="sign-link">
+            회원가입
+          </Link>
+          <div className="vr" />
+          <Link to={"/findid"} className="sign-link">
+            이메일 찾기
+          </Link>
+        </div>
+
+        <div className="text-center">
+          <HorizonLine text="간편 로그인" />
+          <div className='justify-content-between mt-4'>
+            <a id="login_naver" href="#" className='mx-3'>
+              <img src={process.env.PUBLIC_URL + '/btn_naver.png'} height="30" />
+            </a>
+            <a id="login_kakao" href="#" className='mx-3'>
+              <img src={process.env.PUBLIC_URL + '/btn_kakao.png'} height="30" />
+            </a>
+          </div>
+        </div>
+      </Form>
+    </Container>
+  );
+}
+
+export default LogIn;
