a0cf185420aba95f2d748d76451d316a49fcab29
moon-subin <sb020702@gmail.com>
2023-08-12 18:01:33 +0900

    FEAT: 로그인 폼 제출 이벤트 처리 및 헤더 회원가입 링크 추가

--- src/pages/AuthPage/login/login.js
+++ src/pages/AuthPage/login/login.js
@@ -9,12 +9,26 @@ import {
 } from 'react-bootstrap';
 
 function LogIn() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  // 로그인 버튼 클릭 시 호출
+  const loginHandler = (e)=>{
+      e.preventDefault();
+      setEmail(email);
+      setPassword(password);
+  }
+
   return (
     <Container className="d-flex flex-column">
-      <Form className='form-body'>
+      <Form className='form-body'> 
 
         {/* 로그인 */}
-        <Login></Login>
+        <Login
+          setEmail={setEmail}
+          setPassword={setPassword}
+          loginHandler={loginHandler}
+        />
 
         {/* 회원가입 및 이메일 찾기 */}
         <JoinFind></JoinFind>
