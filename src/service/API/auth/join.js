2a4b24290dd4284cda134f59904010fe6a8f0cb0
Rimi <lora3226@inu.ac.kr>
2023-08-14 21:27:59 +0900

    Feat :  localStorage에 AccessToken 저장

--- src/service/API/auth/join.js
+++ src/service/API/auth/join.js
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
 export const joinAPI = async (event,formData) => {
-    event.preventDefault();
+    var response = [];
+    
     try {
-        const response = await axios.post('http://43.202.64.233:3000/api/user/join', formData, { timeout: 5000 });
-        console.log(response.data); // 회원가입 성공 시 서버로부터 온 응답 데이터 처리
+        //axios 연결
+        response = await axios.post('http://43.202.64.233:3000/api/user/join', formData, { timeout: 5000 });
     } catch (error) {
         console.error(error); // 회원가입 실패 시 서버로부터 온 에러 데이터 처리
     }
+
+    // 로컬 스토리지에 저장
+    localStorage.setItem('AccessToken',response.data.AccessToken);
+
+
 }
 
 export default joinAPI;
