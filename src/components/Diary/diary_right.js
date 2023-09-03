c171cba4cc99bcf8961e367d21100077e08f642e
moon-subin <sb020702@gmail.com>
2023-09-03 13:55:50 +0900

    Feat: 오늘의 질문 & 한 줄 일기 작성 전 페이지 퍼블리시

---
+++ src/components/Diary/diary_right.js
@@ -0,0 +1,135 @@
+// 한 줄 일기 컨포넌트
+import React, { useState } from "react";
+import { Form } from 'react-bootstrap';
+
+
+// 기분 선택 컴포넌트
+export function Mood() {
+
+    const [isClicked, setIsClicked] = useState(false);
+    const moodArr =['🥰', '😆', '😐', '😣', '😭'];
+
+    const handleClick = (idx) => {
+        const newArr = Array(moodArr.length).fill(false);
+        newArr[idx] = true;
+        setIsClicked(newArr);
+    }
+
+    return (
+        <div className="mood-option">
+            {moodArr.map((elm, idx) => {
+                return (
+                    <p 
+                        onClick={()=>{handleClick(idx)}}
+                        className={isClicked[idx] ? 'clicked-mood-icon' : 'mood-icon'}
+                    >{elm}</p>
+                );
+            })}
+        </div>
+    );
+}
+
+// 공개 비공개 선택 컴포넌트
+export function IsPrivate() {
+
+    const [isPrivate, setIsPrivate] = useState(false);
+
+    // 공개 선택한 경우
+    const handlePublic = () => {
+        setIsPrivate(false);
+    }
+
+    // 비공개 선택한 경우
+    const handlePrivate = () => {
+        setIsPrivate(true);
+    }
+
+    return (
+        <div className="lock-option">
+            <span className="material-symbols-outlined mx-1 lock-icon" 
+                style={{color: isPrivate ? 'rgba(128, 128, 128, 0.5)' : 'black'}}
+                onClick={()=>{handlePublic()}}>
+                lock
+            </span>
+            <span className="material-symbols-outlined mx-1 lock-icon" 
+                style={{color: isPrivate ? 'black' : 'rgba(128, 128, 128, 0.5)'}}
+                onClick={()=>handlePrivate()}>
+                lock_open_right
+            </span>
+        </div>
+    );
+}
+
+// 입력창 컴포넌트 (제목, 내용)
+export function Input() {
+    return (
+        <div className="input-sz">
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Control as="textarea" rows={1} />
+            </Form.Group>
+        </div>
+    );
+}
+
+// 사진 첨부 컴포넌트
+export function PutImage() {
+    
+    return (
+        <div>
+            <div className="image-div">
+                <span className="material-symbols-outlined add-icon">
+                    add
+                </span>
+            </div>
+        </div>
+    );
+}
+
+// 일기 쓰기 컴포넌트
+export function DiaryDetails(props) {
+
+    return (
+        <div>
+            <div className="select-option">
+                {/* 기분&공개 - 한 줄로 우측 정렬*/}
+                <Mood />
+                <IsPrivate />
+            </div>
+
+            {/* 제목 */}
+            <div className="diary-title">
+                <div className="title-content">
+                    <span className="title-content-font">제목</span>
+                </div>
+                <Input />
+            </div>
+
+            {/* 내용 */}
+            <div className="diary-content">
+                <div className="title-content">
+                    <span className="title-content-font">내용</span>
+                </div>
+                <Input />
+            </div>
+
+            <div className="upload-div">
+                {/* 사진 첨부 */}
+                <div className="title-content">
+                    <span className="title-content-font">사진 첨부</span>
+                </div>
+                <PutImage />
+            </div>
+
+            <div>
+                {/* 저장하기 버튼 */}
+                <div className='save'>
+                    {/* button - 중앙 */}
+                    <button type="submit" className="submitBtn">
+                        저장
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
--- src/pages/DiaryPage/diary.js
+++ src/pages/DiaryPage/diary.js
@@ -1,7 +1,16 @@
 function Diary() {
-    return (
-      <h1>Diary</h1>
-    );
-  };
+  return (
+    <div className='diary-page-segment'>
+      <div className='left'>
+        <TodayQuestionPage />
+      </div>
+      {/* 추후에 설정 변경 */}
+      {/* <div class='vr-center'></div>  */}
+      <div className='right'>
+        <DiaryPage />
+      </div>
+    </div>
+  );
+};
   
   export default Diary;
